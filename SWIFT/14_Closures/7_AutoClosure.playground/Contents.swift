988f34363aff588f3a81ba92f99b3cfa356f236e
msseock <msseock@gmail.com>
2023-02-10 17:25:18 +0900

    Chap13 클로저_자동클로저 실습
    @autoclosure, 자동 클로저의 탈출(@autoclosure @escaping)

--- SWIFT/14_Closures/7_AutoClosure.playground/Contents.swift
+++ SWIFT/14_Closures/7_AutoClosure.playground/Contents.swift
@@ -12,3 +12,27 @@ print(customersInLine.count)    // 4
 // 실제 실행
 print("Now serving \(customerProvider())!") // "Now serving YoangWha!"
 print(customersInLine.count)    // 3
+
+// MARK: 함수의 전달인자로 전달하는 클로저
+func serveCustomer(_ customerProvider: () -> String) {
+    print("Now serving \(customerProvider())!")
+}
+
+serveCustomer( { customersInLine.removeFirst() })   // Now serving YoangWha!
+
+// MARK: 자동 클로저의 사용
+func serveCustomer2(_ customerProvider: @autoclosure () -> String) {
+    print("Now serving \(customerProvider())!!")
+}
+
+serveCustomer2(customersInLine.removeFirst())   // Now serving SungHun!!
+
+// MARK: 자동 클로저의 탈출
+customersInLine = ["minjae", "innoceive", "sopress"]
+
+func returnProvider(_ customerProvider: @autoclosure @escaping () -> String) -> (() -> String) {
+    return customerProvider
+}
+
+let customerProvider2: () -> String = returnProvider(customersInLine.removeFirst())
+print("Now serving \(customerProvider2())!")    // Now serving minjae
