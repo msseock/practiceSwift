3b86aaebac8c4727cf9bc899253ba661c276f2d4
msseock <msseock@gmail.com>
2024-01-22 04:33:47 +0900

    SwiftUI Tutorials: Interfacing with UIKit

--- iOS/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ iOS/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -15,13 +15,18 @@ struct CategoryHome: View {
         NavigationSplitView {
             // Display the categories in Landmarks using a List
             List {
-                // add the image of the first featured landmark to the top of the list
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
-                    // Set the edge insets to zero so the content can extend to the edges of the display
+                // 사진 하나만 있던 버전
+//                // add the image of the first featured landmark to the top of the list
+//                modelData.features[0].image
+//                    .resizable()
+//                    .scaledToFill()
+//                    .frame(height: 200)
+//                    .clipped()
+//                    // Set the edge insets to zero so the content can extend to the edges of the display
+//                    .listRowInsets(EdgeInsets())
+                
+                // 좌우슬라이드 페이지컨트롤 있는 버전
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
                     .listRowInsets(EdgeInsets())
                 
                 // Display the categories in Landmarks using a list
