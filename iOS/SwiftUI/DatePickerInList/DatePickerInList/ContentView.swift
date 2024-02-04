f15d5d73885078ef91e6bdf51b4de5c9d6ced558
msseock <msseock@gmail.com>
2024-02-04 18:22:31 +0900

    DatePicker in List : DatePicker에서 버튼으로 수정
    DatePicker -> DateFormatter를 사용해서 날짜 보여주는 버튼으로 변경

--- iOS/SwiftUI/DatePickerInList/DatePickerInList/ContentView.swift
+++ iOS/SwiftUI/DatePickerInList/DatePickerInList/ContentView.swift
@@ -11,29 +11,45 @@ struct ContentView: View {
     @State private var selectedDate = Date()
     @State private var isDatePickerVisible = false
     
+    // 버튼에 들어갈 날짜 text 만들어주기
+    var dateString: String {
+        // DateFormatter 형식 지정
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        
+        // Date -> String
+        let dateString = dateFormatter.string(from: selectedDate)
+        
+        return dateString
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                DatePicker("날짜 선택", selection: $selectedDate, displayedComponents: .date)
-                    .accentColor(isDatePickerVisible ? .blue : .primary)
-                    .onTapGesture {
-                        isDatePickerVisible.toggle()
-                    }
-                
-                if isDatePickerVisible {
-                    DatePicker("", selection: Binding(
-                        get: { self.selectedDate },
-                        set: { self.selectedDate = $0 }
-                    ), displayedComponents: .date)
-                    .datePickerStyle(.graphical)
+        List {
+            // 첫번째 row: 날짜 선택 button
+            HStack {
+                Text("날짜 선택")
+                Spacer()
+                Button {
+                    isDatePickerVisible.toggle()
+                } label: {
+                    Text(dateString)
                 }
+                // 활성화될 때 accentColor로 변경
+                .foregroundStyle(isDatePickerVisible ? Color.accentColor : Color.primary)
+                // 기존 DatePicker 흉내내기(회색 박스 생김)
+                .buttonStyle(.bordered)
             }
-            .navigationTitle("날짜 선택기")
-            .animation(.smooth)
-
 
             
+            // 두번째 row: 첫번째 row 날짜 부분 누르면 나오는 달력 모양 DatePicker
+            if isDatePickerVisible {
+                DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                // graphical style이 달력 모양
+                .datePickerStyle(.graphical)
+            }
         }
+        // list 추가될 때 뚝뚝 끊기지 않도록 애니메이션 추가
+        .animation(.smooth)
     }
 }
 
