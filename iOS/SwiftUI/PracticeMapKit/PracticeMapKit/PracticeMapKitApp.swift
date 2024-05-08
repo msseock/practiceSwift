a6560f1d9f19bede8689f1030053124ef9035d77
msseock <msseock@gmail.com>
2024-05-08 23:09:39 +0900

    PracticeMapKit: 지도와 annotation 실습

---
+++ iOS/SwiftUI/PracticeMapKit/PracticeMapKit/PracticeMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeMapKitApp.swift
+//  PracticeMapKit
+//
+//  Created by 석민솔 on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
