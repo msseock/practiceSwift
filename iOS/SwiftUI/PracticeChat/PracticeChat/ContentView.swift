d3ce9ebef604b02b01ac0e82451c72c079ebc08c
msseock <msseock@gmail.com>
2024-05-31 16:59:23 +0900

    PracticeChat: 힌트 보기 버튼 UI 위치 수정
    - 앱 구분용 앱아이콘 만들어서 추가
    - 힌트보기 UI 채팅 메시지 입력창이랑 VStack으로 같이 정리

--- iOS/SwiftUI/PracticeChat/PracticeChat/ContentView.swift
+++ iOS/SwiftUI/PracticeChat/PracticeChat/ContentView.swift
@@ -16,28 +16,48 @@ struct ContentView: View {
 
 
     var body: some View {
-        ZStack (alignment: .bottomLeading) {
+        VStack(alignment: .leading) {
+            //MARK: - Chat Message List
+            ScrollViewReader(content: { proxy in
+                ScrollView {
+                    ForEach(chatService.messages) { chatMessage in
+                        //MARK: - Chat Message View
+                        ChatBox(chatMessage: chatMessage)
+                            .padding(.horizontal)
+                    }
+                }
+                // 가장 마지막에 생성된 메시지로 스크롤해주기
+                .onChange(of: chatService.messages) {
+                    guard let recentMessage = chatService.messages.last else { return }
+                    
+                    DispatchQueue.main.async {
+                        withAnimation {
+                            proxy.scrollTo(recentMessage.id, anchor: .bottom)
+                        }
+                    }
+                }
+            })
+            
+            
             VStack(alignment: .leading) {
-                //MARK: - Chat Message List
-                ScrollViewReader(content: { proxy in
-                    ScrollView {
-                        ForEach(chatService.messages) { chatMessage in
-                            //MARK: - Chat Message View
-                            ChatBox(chatMessage: chatMessage)
-                                .padding(.horizontal)
-                        }
-                    }
-                    // 가장 마지막에 생성된 메시지로 스크롤해주기
-                    .onChange(of: chatService.messages) {
-                        guard let recentMessage = chatService.messages.last else { return }
-                        
-                        DispatchQueue.main.async {
-                            withAnimation {
-                                proxy.scrollTo(recentMessage.id, anchor: .bottom)
-                            }
-                        }
-                    }
+                // MARK: - Hint Button
+                Button(action: {
+                    print("힌트 보기")
+                    showHint.toggle()
+                    // TODO: 힌트 보기 호출하기
+                    
+                }, label: {
+                    Image("ic_bulb")
+                    Text(showHint ? "힌트 숨기기" : "힌트 보기")
                 })
+                .foregroundStyle(Color.white)
+                // TODO: orange-medium으로 색이름 대체
+                .padding()
+                .background(showHint ? Color(red: 0.57, green: 0.59, blue: 0.6) : Color(red: 0.99, green: 0.56, blue: 0.3))
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .shadow(color: Color(red: 0.24, green: 0.26, blue: 0.27).opacity(0.12), radius: 4, x: 0, y: 4)
+                .padding(.bottom, 0)
+                .padding(.leading, 10)
                 
                 //MARK: - Input Field
                 HStack {
@@ -64,40 +84,23 @@ struct ContentView: View {
                     }
                 }
                 .padding()
-                .background(Color.white)
+            .background(Color.white)
+            }
 
-            }
-            .foregroundStyle(.white)
-            .background {
-                //MARK: - Background
-                ZStack {
-                    Color(red: 247/255, green: 249/255, blue: 250/255)
-                }
-                .ignoresSafeArea()
-                .onTapGesture {
-                    isFocused = false
-                }
-            }
-            
-            // 힌트 보기 버튼
-            Button(action: {
-                print("힌트 보기")
-                showHint.toggle()
-                // TODO: 힌트 보기 호출하기
-                
-            }, label: {
-                Image("ic_bulb")
-                Text(showHint ? "힌트 숨기기" : "힌트 보기")
-            })
-            .foregroundStyle(Color.white)
-            // TODO: orange-medium으로 색이름 대체
-            .padding()
-            .background(showHint ? Color(red: 0.57, green: 0.59, blue: 0.6) : Color(red: 0.99, green: 0.56, blue: 0.3))
-            .clipShape(RoundedRectangle(cornerRadius: 40))
-            .shadow(color: Color(red: 0.24, green: 0.26, blue: 0.27).opacity(0.12), radius: 4, x: 0, y: 4)
-            .padding(.bottom, 100)
-            .padding(.leading, 28)
         }
+        .foregroundStyle(.white)
+        .background {
+            //MARK: - Background
+            ZStack {
+                Color(red: 247/255, green: 249/255, blue: 250/255)
+            }
+            .ignoresSafeArea()
+            .onTapGesture {
+                isFocused = false
+            }
+        }
+        
+        
 
     }
 
